423ff05f25019b595ca0289f0aff3005ca918160
In Code We Trust <njderenne@gmail.com>
2020-09-16 22:25:41 -0500

    added user prompt

---
+++ Develop/script.js
@@ -0,0 +1,17 @@
+// Assignment code here
+var passwordLength = window.prompt("How many characters would you like your password to be? (Enter a number of at least 8)")
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);

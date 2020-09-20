50a7a0c7fce526e3d44f102782a33010db6c8daf
In Code We Trust <njderenne@gmail.com>
2020-09-19 21:36:10 -0500

    created a list of values for password

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,39 @@
 // Assignment code here
-generatePassword = function(){ 
-  passwordLength = window.prompt("How many characters would you like your password to be? (Enter a number of at least 8.")
-  return "Your password will be " + passwordLength + " characters long."
+var value = "";
+var letterChoice = function() {
+  value = "";
+  var lowerCase = window.confirm("Do you want to include lower case letters?");
+  if (lowerCase) {
+    value = value + "abcdefghijklmnopqrstuvwxyz"
+  }
+
+  var upperCase = window.confirm("Do you want to include upper case letters?");
+  if (upperCase) {
+    value = value + "ABCEDFGHIJKLMNOPQRSTUVWXYZ"
+  }
+
+  var numbers = window.confirm("Do you want to include numbers?");
+  if (numbers) {
+    value = value + "1234567890"
+  }
+  var specialCharacters = window.confirm("Do you want to include special characters?");
+  if (specialCharacters) {
+    value = value + " !#$%&()*+,-./:;<=>?@[]^_{}`~"
+  }
+  if (lowerCase === false && upperCase === false && numbers === false && specialCharacters === false) {
+    window.alert("You need to select at least one character type to generate a password");
+    letterChoice();
+  }
+  return value
+}
+
+var generatePassword = function(){ 
+  var passwordLength = window.prompt("How many characters would you like your password to be? (Enter a number of at least 8.")
+  while (passwordLength < 8 || passwordLength > 128){
+    passwordLength = window.prompt("Please choose a number between 8 and 128")
+  }
+  letterChoice();
+  return "Your password will be " + passwordLength + " characters long with value options of " + value + "."
 };
 
 // Get references to the #generate element
@@ -14,6 +46,10 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+var passwordInfo = {
+  
 }
 
 // Add event listener to generate button
